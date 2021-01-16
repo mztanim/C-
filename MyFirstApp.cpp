51a96668ee4b60157a268a5e84d72705246fa860
mztanim <mztaim@gmail.com>
2021-01-16 23:08:41 +0200

    first commit

---
+++ MyFirstApp.cpp
@@ -0,0 +1,14 @@
+// MyFirstApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "My name is Tanim.\n";
+    cout << "Today is Friday.\n";
+    cout << " Hi, come here\n";
+    system("pause>0");
+}
+
